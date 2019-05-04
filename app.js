dc92d01fc20e08a93a0481f69c491cb6564a6a41
Alberto Garibay <garib112@mail.chapman.edu>
2019-05-04 13:24:03 -0700

    Table functionality

--- app.js
+++ app.js
@@ -241,56 +241,106 @@ app.post("/insertDB", (req, res) => {
 })
 
 function intializeTables() {
-  var createUSWarehouse = 'CREATE TABLE IF NOT EXISTS USWarehouse (' +
-                              'WarehouseId INTEGER auto_increment not null,' +
-                              'SKU INTEGER UNIQUE,' +
-                              'PRIMARY KEY (WarehouseId),' +
-                              'Description varchar(25),' +
-                              'SellableOnHand INTEGER,' +
-                              'OpenPOQuantity INTEGER,' +
-                              'MOQ INTEGER,' +
-                              'LeadTime INTEGER,' +
-                              'BackOrders INTEGER);';
+  var createUSWarehouse = `CREATE TABLE IF NOT EXISTS USWarehouse (
+                              WarehouseId INTEGER auto_increment not null,
+                              SKU INTEGER UNIQUE,
+                              PRIMARY KEY (WarehouseId),
+                              Description varchar(25),
+                              SellableOnHand INTEGER,
+                              OpenPOQuantity INTEGER,
+                              MOQ INTEGER,
+                              LeadTime INTEGER,
+                              BackOrders INTEGER)`;
 
-  // var createTotalSales = 'CREATE TABLE IF NOT EXISTS TotalSales (' +
-  //                           'SalesId INTEGER auto_increment not null,' +
-  //                           'PRIMARY KEY (SalesId),' +
-  //                           'OneMonthSales INTEGER,' +
-  //                           'ThreeMonthSales INTEGER,' +
-  //                           'SixMonthSales INTEGER);';
+  // var createTotalSales = `CREATE TABLE IF NOT EXISTS TotalSales (
+  //                           SalesId INTEGER auto_increment not null,
+  //                           PRIMARY KEY (SalesId),
+  //                           OneMonthSales INTEGER,
+  //                           ThreeMonthSales INTEGER,
+  //                           SixMonthSales INTEGER)`;
 
-  var createDimensionTable = 'CREATE TABLE IF NOT EXISTS DimensionTable (' +
-                              'DimensionId INTEGER auto_increment not null,' +
-                              'PRIMARY KEY (DimensionId),' +
-                              'SKU INTEGER,' +
-                              'Fob_SH FLOAT,' +
-                              'L_cm INTEGER,' +
-                              'W_cm INTEGER,' +
-                              'H_cm INTEGER,' +
-                              'G_W_kg INTEGER,' +
-                              'N_W_kg INTEGER);';
+  var createDimensionTable = `CREATE TABLE IF NOT EXISTS DimensionTable (
+                              DimensionId INTEGER auto_increment not null,
+                              PRIMARY KEY (DimensionId),
+                              SKU INTEGER,
+                              Fob_SH FLOAT,
+                              L_cm INTEGER,
+                              W_cm INTEGER,
+                              H_cm INTEGER,
+                              G_W_kg INTEGER,
+                              N_W_kg INTEGER)`;
 
-  var createPackagingTable = 'CREATE TABLE IF NOT EXISTS PackagingTable (' +
-                              'PackagingId INTEGER auto_increment not null,' +
-                              'PRIMARY KEY (PackagingId),' +
-                              'PackagingType varchar(30),' +
-                              'PalletDim varchar(30),' +
-                              'PalletCtns INTEGER,' +
-                              'PalletQty INTEGER,' +
-                              'PalletWeight INTEGER,' +
-                              'CartonQuantity INTEGER);';
+  var createPackagingTable = `CREATE TABLE IF NOT EXISTS PackagingTable (
+                              PackagingId INTEGER auto_increment not null,
+                              PRIMARY KEY (PackagingId),
+                              SKU INTEGER,
+                              PackagingType varchar(30),
+                              PalletDim varchar(30),
+                              PalletCtns INTEGER,
+                              PalletQty INTEGER,
+                              PalletWeight INTEGER,
+                              CartonQuantity INTEGER)`;
 
-  var TestTable = 'CREATE TABLE IF NOT EXISTS TestTable (' +
-                      'SKU INTEGER UNIQUE,' +
-                      'PRIMARY KEY (SKU),' +
-                      'Test1 varchar(20),' +
-                      'Test2 varchar(20),' +
-                      'Test3 varchar(20),' +
-                      'Test4 varchar(20),' +
-                      'Test5 varchar(20),' +
-                      'Test6 varchar(20))';
-  getConnection().query(TestTable, function(err,result) {
-    if (err) throw err;
-    console.log('Table Created!');
+  var createShippingTable = `CREATE TABLE IF NOT EXISTS ShippingTable (
+                              ShippingId INTEGER auto_increment not null,
+                              PRIMARY KEY (ShippingId),
+                              SKU INTEGER,
+                              ShippingDate varchar(10),
+                              ShippingPONumber INTEGER,
+                              ShippingQty INTEGER,
+                              IncostUnit INTEGER,
+                              ShippingTotalAmt FLOAT,
+                              Received varchar(3),
+                              ShippingDateReceived varchar(10))`;
+
+  var createSalesTable = `CREATE TABLE IF NOT EXISTS SalesTable (
+                              SalesId INTEGER auto_increment not null,
+                              PRIMARY KEY (SalesId),
+                              SKU INTEGER,
+                              Vendor varchar(30),
+                              SalesPONumber INTEGER,
+                              SalesQty INTEGER,
+                              SalesDate varchar(10),
+                              SalesAmt FLOAT)`;
+
+  // var TestTable = 'CREATE TABLE IF NOT EXISTS TestTable (' +
+  //                     'SKU INTEGER UNIQUE,' +
+  //                     'PRIMARY KEY (SKU),' +
+  //                     'Test1 varchar(20),' +
+  //                     'Test2 varchar(20),' +
+  //                     'Test3 varchar(20),' +
+  //                     'Test4 varchar(20),' +
+  //                     'Test5 varchar(20),' +
+  //                     'Test6 varchar(20))';
+
+  getConnection().query(createUSWarehouse, function(err,result) {
+    if (err) {
+      console.log(err);
+    }
+    console.log('USWarehouse Table Created!');
+  });
+  getConnection().query(createDimensionTable, function(err,result) {
+    if (err) {
+      console.log(err);
+    }
+    console.log('Dimension Table Created!');
+  });
+  getConnection().query(createPackagingTable, function(err,result) {
+    if (err) {
+      consol.log(err);
+    }
+    console.log('Packaging Table Created!');
+  });
+  getConnection().query(createShippingTable, function(err,result) {
+    if (err) {
+      console.log(err);
+    }
+    console.log('Shipping Table Created!');
+  });
+  getConnection().query(createSalesTable, function(err,result) {
+    if (err) {
+      console.log(err);
+    }
+    console.log('Sales Table Created!');
   });
 }
