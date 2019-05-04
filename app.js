e1f1e5ac90db18c50dbbc4be22405208dd48e11e
Alberto Garibay <garib112@mail.chapman.edu>
2019-05-04 12:45:37 -0700

    Created tables functionality

--- app.js
+++ app.js
@@ -41,6 +41,11 @@ app.get("/html/index.html", (req, res) => {
   res.send("Hello from root")
 })
 
+app.get("/product-search", (req, res) => {
+  console.log("Responding to root route")
+  res.send("Hello from root")
+})
+
 app.post('/auth', function(request, response) {
 	var email = request.body.email;
 	var password = request.body.password;
@@ -49,6 +54,7 @@ app.post('/auth', function(request, response) {
 			if (results.length > 0) {
 				request.session.loggedin = true;
 				request.session.email = email;
+        intializeTables();
 				response.redirect('/home.html');
 			} else {
         response.redirect('/index.html')
@@ -128,6 +134,41 @@ app.listen(3030, () => {
 
 // -> Import CSV File to MySQL database
 function importCsvData2MySQL(filePath){
+    var sku = [];
+    var Description = [];
+    var SellableOnHand = [];
+    var OpenPOqty = [];
+    var OneMonthSales = [];
+    var ThreeMonthSales = [];
+    var SixMonthSales = [];
+    var BackOrder = [];
+    var LeadTime = [];
+    var MoQ = [];
+    var FobSH = [];
+    var PackagingType = [];
+    var L_cm = [];
+    var W_cm = [];
+    var H_cm = [];
+    var GW_kg = [];
+    var NW_kg = [];
+    var Carton_qty = [];
+    var Pallet_Dim_cm = [];
+    var Pallet_Ctns = [];
+    var Pallet_qty = [];
+    var Pallet_WG_qty = [];
+    var Shipping_Date = [];
+    var Shipping_PO = [];
+    var Shipping_Qty = [];
+    var Cost_Unit = [];
+    var Shipping_Amt = [];
+    var Shpmt_Received = [];
+    var Shpmt_Date_Received = [];
+    var Vendor = [];
+    var Sales_PO = [];
+    var Sales_Qty = [];
+    var Sales_Date = [];
+    var Sales_Amt = [];
+
     let stream = fs.createReadStream(filePath, 'utf-8');
     let csvData = [];
     let csvStream = csv
@@ -138,6 +179,45 @@ function importCsvData2MySQL(filePath){
         .on("end", function () {
             // Remove Header ROW
             csvData.shift();
+            //loop through each item in array and save them to appropriate sub-array
+            csvData.forEach(function(entry) {
+              sku = csvData[0];
+              Description = csvData[1];
+              SellableOnHand = csvData[2];
+              OpenPOqty = csvData[3];
+              OneMonthSales = csvData[4];
+              ThreeMonthSales = csvData[5];
+              SixMonthSales = csvData[6];
+              BackOrder = csvData[7];
+              LeadTime = csvData[8];
+              MoQ = csvData[9];
+              FobSH = csvData[10];
+              PackagingType = csvData[11];
+              L_cm = csvData[12];
+              W_cm = csvData[13];
+              H_cm = csvData[14];
+              GW_kg = csvData[15];
+              NW_kg = csvData[16];
+              Carton_qty = csvData[17];
+              Pallet_Dim_cm = csvData[18];
+              Pallet_Ctns = csvData[19];
+              Pallet_qty = csvData[20];
+              Pallet_WG_qty = csvData[21];
+              Shipping_Date = csvData[22];
+              Shipping_PO = csvData[23];
+              Shipping_Qty = csvData[24];
+              Cost_Unit = csvData[25];
+              Shipping_Amt = csvData[26];
+              Shpmt_Received = csvData[27];
+              Shpmt_Date_Received = csvData[28];
+              Vendor = csvData[29];
+              Sales_PO = csvData[30];
+              Sales_Qty = csvData[31];
+              Sales_Date = csvData[32];
+              Sales_Amt = csvData[33];
+            });
+            console.log(sku[0]);
+            console.log(sku);
             // Open the MySQL connection
             getConnection().connect((error) => {
                 if (error) {
@@ -159,3 +239,58 @@ app.post("/insertDB", (req, res) => {
   var csvfile = req.body.csv_file;
   importCsvData2MySQL(csvfile)
 })
+
+function intializeTables() {
+  var createUSWarehouse = 'CREATE TABLE IF NOT EXISTS USWarehouse (' +
+                              'WarehouseId INTEGER auto_increment not null,' +
+                              'SKU INTEGER UNIQUE,' +
+                              'PRIMARY KEY (WarehouseId),' +
+                              'Description varchar(25),' +
+                              'SellableOnHand INTEGER,' +
+                              'OpenPOQuantity INTEGER,' +
+                              'MOQ INTEGER,' +
+                              'LeadTime INTEGER,' +
+                              'BackOrders INTEGER);';
+
+  // var createTotalSales = 'CREATE TABLE IF NOT EXISTS TotalSales (' +
+  //                           'SalesId INTEGER auto_increment not null,' +
+  //                           'PRIMARY KEY (SalesId),' +
+  //                           'OneMonthSales INTEGER,' +
+  //                           'ThreeMonthSales INTEGER,' +
+  //                           'SixMonthSales INTEGER);';
+
+  var createDimensionTable = 'CREATE TABLE IF NOT EXISTS DimensionTable (' +
+                              'DimensionId INTEGER auto_increment not null,' +
+                              'PRIMARY KEY (DimensionId),' +
+                              'SKU INTEGER,' +
+                              'Fob_SH FLOAT,' +
+                              'L_cm INTEGER,' +
+                              'W_cm INTEGER,' +
+                              'H_cm INTEGER,' +
+                              'G_W_kg INTEGER,' +
+                              'N_W_kg INTEGER);';
+
+  var createPackagingTable = 'CREATE TABLE IF NOT EXISTS PackagingTable (' +
+                              'PackagingId INTEGER auto_increment not null,' +
+                              'PRIMARY KEY (PackagingId),' +
+                              'PackagingType varchar(30),' +
+                              'PalletDim varchar(30),' +
+                              'PalletCtns INTEGER,' +
+                              'PalletQty INTEGER,' +
+                              'PalletWeight INTEGER,' +
+                              'CartonQuantity INTEGER);';
+
+  var TestTable = 'CREATE TABLE IF NOT EXISTS TestTable (' +
+                      'SKU INTEGER UNIQUE,' +
+                      'PRIMARY KEY (SKU),' +
+                      'Test1 varchar(20),' +
+                      'Test2 varchar(20),' +
+                      'Test3 varchar(20),' +
+                      'Test4 varchar(20),' +
+                      'Test5 varchar(20),' +
+                      'Test6 varchar(20))';
+  getConnection().query(TestTable, function(err,result) {
+    if (err) throw err;
+    console.log('Table Created!');
+  });
+}
